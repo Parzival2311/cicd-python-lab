31295613b2c9153103305c0e80cdc89726daaee0
Parzival2311 <malayseth2311@gmail.com>
2026-04-17 21:03:59 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,5 +6,5 @@ def hello_world(): [cite: 148]
     # This string is what you will update to test your automated deployment
     return "<h1>CI/CD Pipeline is Live!</h1><p>Version 1.0: Initial Deployment.</p>" [cite: 149]
 
-if __name__ == '__main__': [cite: 150]
+if __name__ == "__main__": [cite: 150]
     application.run(debug=True) [cite: 151]
